e3372a582adad31798106fa5601e77f50608f3ad
rmindboy <l.toledo@duocuc.cl>
2025-10-20 17:08:03 -0300

    commit separacion tarjetas main y ventana juegos

--- src/assets/components/Games.jsx
+++ src/assets/components/Games.jsx
@@ -1,15 +1,26 @@
-
-import { GameCard } from "./MainCards";
-import React, { useState } from "react";
 import "../css/Games.css";
 
+export function GamesCard({ href, img, alt, title, subtitle, children }) {
+  return (
+    <a href={href} className="games-card-link">
+      <section className="games-card">
+        <img src={img} alt={alt} className="games-img" />
+        <div className="games-info">
+          <h2>{title}</h2>
+          <span className="games-subtitle">{subtitle}</span>
+          <p className="games-desc">{children}</p>
+        </div>
+      </section>
+    </a>
+  );
+}
 
 export function Games(){
 return (
     <main className="main-menu">
       <h1 className="main-title">Todos los Juegos</h1>
 
-      <GameCard
+      <GamesCard
         href="blackjack.html"
         img="src/assets/imgs/blackjack.png"
         alt="test"
@@ -20,7 +31,7 @@ return (
         <br />
         <br />
         TESTESTESTESTESTESTESTESTESTESTESTESTES
-      </GameCard>
+      </GamesCard>
 
     </main>
   );
